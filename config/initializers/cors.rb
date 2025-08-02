628c716609cfbf547367bd44814bc0a4e25c0797
kanno <k.takumi114@gmail.com>
2025-08-02 09:37:48 +0900

    add:疎通確認のための設定追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,11 +9,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins [
       'http://localhost:3000',
+      '"http://front:3000"',
       'https://next-poc-front.vercel.app',
-      %r{\Ahttps://next-poc-front-git-.*\.vercel\.app\z}
+      %r{\Ahttps://next-poc-front-[\w\d]+-kannos-projects-41dbdf7a\.vercel\.app\z}
     ]
     resource "*",
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
