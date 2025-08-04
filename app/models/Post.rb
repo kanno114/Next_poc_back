76037d7ba1e6b9c15f6895593f766d316335c0b1
kanno <k.takumi114@gmail.com>
2025-08-04 16:53:54 +0900

    add:各モデル作成

---
+++ app/models/Post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :body, presence: true, length: { maximum: 1000 }
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password validations: false
 
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6 }, if: -> { password.present? }
 end
